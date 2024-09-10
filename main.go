79b2cce9e9cc1f678819fb76494d3896eedc34ab
ivan <ivan@colektia.com>
2024-09-10 20:35:46 -0300

    remove main xd

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
-package main
+package greetings
 
 import "fmt"
 
 func Hello(name string) string {
+	goodEvening()
 	res := fmt.Sprintf("Hi %v, bienvenidooo !!", name)
 	return res
-}
-
-func main() {
-	fmt.Println(Hello("Ivan"))
-	goodEvening()
 }
